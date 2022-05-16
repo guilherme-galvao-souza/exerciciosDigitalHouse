abb8f7947a901b14f922d3073b65b7bf7f9c049e
Guilherme Souza <delayggz@hotmail.com>
2022-05-15 21:43:02 -0300

    Exercício Verificador de Idade

---
+++ Front-EndII/aula03/exercicios.js
@@ -0,0 +1,14 @@
+let idade = parseInt(prompt("Introduza sua idade")); 
+
+if(isNaN(idade) || idade == 0){
+    alert('[ERRO] Os dados digitados estão incorretos, tente novamente.')
+     
+}else if(idade<18){
+    alert('Você é menor de idade')
+}
+else{
+    alert('Você é maior de idade')
+}
+
+
+
