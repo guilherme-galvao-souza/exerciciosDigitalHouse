cf3eddeda8abde5361450829d8a7429b138ed763
guilherme-galvao-souza <delayggz@hotmail.com>
2022-05-15 23:07:53 -0300

    Ciclos For In e For Of

--- Front-EndII/aula03/ciclosEspecificos/ciclosForIn-ForOf.js
+++ Front-EndII/aula03/ciclosEspecificos/ciclosForIn-ForOf.js
@@ -4,12 +4,12 @@ let pessoa ={
     idade:'19',
     emprego:'Desempregado'
 }
-for(let dadosCliente in pessoa){
+for(let dadosCliente in pessoa){ //é utilizado apenas para objetos
     console.log(dadosCliente+':'+pessoa[dadosCliente])
 }
 
 let filmes = ['Bad guys','Homem - Aranha no AranhaVerso', 'Kimi no na wa','Koe no Katachi','A viagem de Chihiro']
-for(let todosFilmes of filmes){
+for(let todosFilmes of filmes){ //é utilizado para objetos iteráveis, por exemplo, matrizes e Strings.
     console.log(todosFilmes)
 }
 // for(let i = 0 ; i<filmes.length; i++){
